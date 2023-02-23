f1f71e2f587d81f5f3714dd85cf7d8056b067cd9
ZacharyWittmann <zawittmann@miners.utep.edu>
2023-02-23 03:32:08 -0700

    2-23-23 Changes

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -5,23 +5,68 @@
 /* Return true (non-zero) if c is a whitespace characer
    ('\t' or ' ').  
    Zero terminators are not printable (therefore false) */
-int space_char(char c);
+int space_char(char c)
+{
+  if (c == ' ' || c == '\t' || c == '\0')
+	    return 1;
+  else
+    return 0;
+}
 
 /* Return true (non-zero) if c is a non-whitespace 
    character (not tab or space).  
    Zero terminators are not printable (therefore false) */ 
-int non_space_char(char c);
+int non_space_char(char c)
+{
+  if (c == ' ' || c == '\t' || c == '\0')
+	    return 0;
+  else
+    return 1;
+}
 
 /* Returns a pointer to the first character of the next 
    space-separated word in zero-terminated str.  Return a zero pointer if 
    str does not contain any words. */
-char *word_start(char *str); 
+char *word_start(char *str)
+{
+  while(space_char(*str))
+    *str++;
+  return str;
+}
 
 /* Returns a pointer terminator char following *word */
-char *word_terminator(char *word);
+char *word_terminator(char *word)
+{
+  while(space_char(*word))
+    *word++;
+
+  while(non_space_char(*word))
+    *word++;
+  
+  return word;
+}
 
 /* Counts the number of words in the string argument. */
-int count_words(char *str);
+int count_words(char *str)
+{
+  int count = 0;
+  char *p = str;
+
+  if (*p == '\0')
+    return count;
+
+  while (*p)
+    {
+      while(space_char(*p))
+	*p++;
+
+      for (; *p != '\0'; p++)
+	if (*p == ' ' && *(p + 1) != ' ')
+	  count++;
+      count++;
+    }
+  return count;
+}
 
 /* Returns a fresly allocated new zero-terminated string 
    containing <len> chars from <inStr> */
